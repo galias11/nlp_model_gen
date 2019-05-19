7b3a35d63a317dd93765d5c378c7f0e076acc981
Gerardo Alias <gerardo.alias@globant.com>
2019-05-19 18:48:14 -0300

    task - Patch import cat command

--- nlp_model_gen/base.py
+++ nlp_model_gen/base.py
@@ -3,6 +3,6 @@ import os
 
 CURRENT_BASE_PATH = os.path.dirname(os.path.realpath(__file__))
 EXPORT_FORMAT = 'zip'
-FILE_JOIN_OPERATOR = 'cat' if not os.name == 'nt' else 'type'
+FILE_JOIN_OPERATOR = 'cat'
 REMOTE_MODEL_SOURCE = {'path':'https://github.com/galias11', 'remote': True}
 SPLIT_FILE_OPERATOR = 'split -a 1 -b'
